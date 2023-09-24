c06f27f5467489f5d3e28a87286ed9f0c511559d
bandaksergey <145925332+bandaksergey@users.noreply.github.com>
2023-09-24 17:54:49 +0000

    firstapp.java file

--- firstapp.java
+++ firstapp.java
@@ -1,4 +1,5 @@
 public class firstapp {
-  public static void main(String[] args
-  System.out.println("my word!");
+  public static void main(String[] args){
+    System.out.println("Hello Ukraine");
+  }
 }
